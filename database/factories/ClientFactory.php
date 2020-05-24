54494c268c7d6f06d219651d579410236e233122
Dayana Natasha <dayana.natasha2017@gmail.com>
2020-05-24 18:45:22 -0300

    Geração de CPFs aleatórios

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -5,9 +5,13 @@
 use App\AppClient;
 use Faker\Generator as Faker;
 
+require_once __DIR__ . '/../faker_data/document_number.php';
+
 $factory->define(AppClient::class, function (Faker $faker) {
+    $cpfs = cpfs();
     return [
         'name' => $faker->name,
+        'document_number' => array_rand($cpfs, 1),
         'email' => $faker->email,
         'phone' => $faker->phoneNumber,
         'defaulter' => rand(0, 1),
