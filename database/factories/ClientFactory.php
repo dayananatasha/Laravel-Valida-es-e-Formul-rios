4e20fde4356a258fd1e6b7b91adc17e004c20eea
Dayana Natasha <dayana.natasha2017@gmail.com>
2020-05-26 23:05:57 -0300

    Correções

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -2,22 +2,23 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\AppClient;
+use App\Client;
 use Faker\Generator as Faker;
 
 require_once __DIR__ . '/../faker_data/document_number.php';
 
-$factory->define(AppClient::class, function (Faker $faker) {
+$factory->define(Client::class, function (Faker $faker) {
     $cpfs = cpfs();
     return [
         'name' => $faker->name,
-        'document_number' => array_rand($cpfs, 1),
+        'document_number' => $cpfs[array_rand($cpfs, 1)],
         'email' => $faker->email,
         'phone' => $faker->phoneNumber,
         'defaulter' => rand(0, 1),
         'date_birth' => $faker->date(),
         'sex' => rand(1, 10) % 2 == 0 ? 'm' : 'f',
         'physical_disability' => rand(1, 10) % 2 == 0 ? $faker->word : null,
+        'marital_status' => 1
 
     ];
 });
