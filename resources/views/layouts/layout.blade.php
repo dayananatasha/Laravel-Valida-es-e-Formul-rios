cf888e585c1c8ec3f116c1d4dcf27312a382ea9e
Dayana Natasha <dayana.natasha2017@gmail.com>
2020-05-26 23:22:19 -0300

    Blade - Layouts

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head></head>
+        <link rel="stylesheet" href="/css/app.css"/>
+    <body>
+        <div class="row">
+            <div class="container">
+            <h1>Laravel: Formulários e Validações</h1>
+            @yield('content')
+            </div>
+        </div>
+        <script type="text/javascript" src="/js/app.js"></script>
+    </body>
+</html>
